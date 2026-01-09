80794c3c0fa94ebd80580accdb576cee2365ecea
Damewan Bareh <damewanbareh86@gmail.com>
2026-01-09 23:39:46 +0530

    Adjusted the editor to the middle

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div><RichTextEditor /></div>  
+    <div className="max-w-3xl mx-auto py-8"><RichTextEditor /></div>  
   );
 }
